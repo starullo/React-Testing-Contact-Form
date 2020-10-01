0f122901b2dd5375a4b15daad7a1dce08b57dcee
Sam Tarullo <sam@Sams-Air.attlocal.net>
2020-10-01 17:32:59 -0500

    finished I think?

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,34 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("renders App without crashing", () => {
-  render(<App />);
-});
+
+
+describe('Tests Form', () => {
+  test("renders App without crashing", async () => {
+    render(<App />);
+  
+    const firstNameInput = screen.getByLabelText(/first name/i);
+  
+     const lastNameInput = screen.getByLabelText(/last name/i);
+  
+    const emailInput = screen.getByLabelText(/email/i);
+  
+    const messageInput = screen.getByLabelText(/message/i);
+  
+    const button = screen.getByRole(/button/i);
+
+    fireEvent.change(firstNameInput, {target: {name: 'firstName', value: 'Sam'}});
+
+    fireEvent.change(lastNameInput, {target: { value: 'Tarullo'}});
+
+    fireEvent.change(emailInput, {target: {value: 'starullo87@gmail.com'}});
+
+    fireEvent.change(messageInput, {target: {value: 'This is the greatest message of all time'}});
+
+    fireEvent.click(button);
+
+    await screen.findByTestId(/json/i);
+
+  });
+})
